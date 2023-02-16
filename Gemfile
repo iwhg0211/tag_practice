fbc87f9402076a31416ee1e839425c667ff5ab1f
EC2 Default User <ec2-user@ip-172-31-44-123.ap-northeast-1.compute.internal>
2023-02-16 01:54:54 +0000

    [add]post model,posts controller

--- Gemfile
+++ Gemfile
@@ -71,4 +71,4 @@ group :test do
   gem "webdrivers"
 end
 
-gem 'acts-as-taggable-on', '~> 6.0'
+gem 'acts-as-taggable-on'
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all
+    @tags = Post.tag_counts_on(:tags).most_used(20)    # タグ一覧表示
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+    @tags = @post.tag_counts_on(:tags)    # 投稿に紐付くタグの表示
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :content, :tag_list)
+  end
+  
+end
